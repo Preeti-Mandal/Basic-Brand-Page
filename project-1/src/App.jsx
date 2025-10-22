1c8e2b9cd32412318e7a7dc27190a5a8cd79a17b
Preeti-Mandal <preetimandal001@gmail.com>
2025-10-22 13:16:05 +0530

    Restored project

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';  //global styles
+import NavBar from './components/Navigation';
+import HeroSection from './components/Hero';
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <HeroSection/>
+    </div>
+  );
+};
+
+export default App;
